7309b7b8ce7323ce29b2c78c2174ab720da4d0fa
ERIKO <erik.valenciac@unac.edu.co>
2026-07-07 19:23:39 -0500

    feat: debug console log v2

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -59,6 +59,7 @@ export async function POST(request: NextRequest) {
       path: "/",
       maxAge: 60 * 60 * 24 * 30,
     });
+    console.log(response.cookies.getAll());
 
     return response;
 
